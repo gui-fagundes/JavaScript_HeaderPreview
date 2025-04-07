274d1a65b3bbfcdd500fd496148595ca76fb1607
Guilherme Fagundes <guilhermelemosfurg@gmail.com>
2025-04-06 23:59:53 -0300

    feat: add menu links

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-document.getElementById("bgColor").addEventListener("change", (e) => {
+document.getElementById("bgColor").addEventListener("input", (e) => {
   e.preventDefault();
   const bgColor = document.getElementById("bgColor").value;
   const headerPreview = document.getElementById("headerPreview");
   headerPreview.style.backgroundColor = bgColor;
 });
 
-document.getElementById("textColor").addEventListener("change", (e) => {
+document.getElementById("textColor").addEventListener("input", (e) => {
   e.preventDefault();
   const textColor = document.getElementById("textColor").value;
   headerTitle.style.color = textColor;
@@ -25,19 +25,62 @@ document.getElementById("fontWeight").addEventListener("change", (e) => {
 
 document.getElementById("fontItalic").addEventListener("change", (e) => {
   const isChecked = document.getElementById("fontItalic").checked;
-  if(isChecked) {
+  if (isChecked) {
     headerTitle.style.fontStyle = "italic";
   } else {
-    headerTitle.style.fontStyle = "normal"
+    headerTitle.style.fontStyle = "normal";
   }
-})
+});
 
-document.getElementById("headerText").addEventListener("change", (e) => {
+document.getElementById("headerText").addEventListener("input", (e) => {
   e.preventDefault();
   const headerText = document.getElementById("headerText").value;
   headerTitle.textContent = headerText;
 });
 
+document.getElementById("addButton").addEventListener("click", (e) => {
+  e.preventDefault();
+  const menu = document.getElementById("menuLinks");
+  const novoItem = document.createElement("li");
+  const novoLink = document.createElement("a");
+  novoLink.href = "";
+  novoLink.textContent = document.getElementById("linkText").value;
+  novoLink.target = "blank";
+  novoLink.className = "menuLink";
+  novoItem.appendChild(novoLink);
+  menu.appendChild(novoItem);
+  document.getElementById("linkText").value = "";
+});
+
+document.getElementById("menuLinkFontSize").addEventListener("change", (e) => {
+  e.preventDefault();
+  const menuLinkFontSize = document.getElementById("menuLinkFontSize").value;
+  headerMenu.style.fontSize = `${menuLinkFontSize}px`;
+});
+
+document.getElementById("linkColor").addEventListener("input", (e) => {
+  e.preventDefault();
+  const linksColor = document.getElementById("linkColor").value;
+  const menuLinks = document.getElementsByClassName("menuLink");
+  for(var i = 0 ; i <menuLinks.length ; i++) {
+    menuLinks[i].style.color = linksColor;
+  }
+});
+
+document.getElementById("searchBox").addEventListener("change", (e) => {
+  e.preventDefault();
+  const searchBar = document.getElementById("searchBox").value;
+  const search = document.getElementById("headerSearch");
+  if (searchBar === "true") {
+    search.style.display = "block";
+    document.getElementById("headerPreview").style.justifyContent =
+      "space-between";
+  } else {
+    search.style.display = "none";
+    document.getElementById("headerPreview").style.justifyContent =
+      "space-around";
+  }
+});
 document.getElementById("bannerUpload").addEventListener("change", (e) => {
   e.preventDefault();
   const bannerUpload = document.getElementById("bannerUpload").files[0];
@@ -45,7 +88,7 @@ document.getElementById("bannerUpload").addEventListener("change", (e) => {
     const reader = new FileReader();
     reader.onload = function (e) {
       headerPreview.style.backgroundImage = `url(${e.target.result})`;
-      headerPreview.style.backgroundColor = ""; 
+      headerPreview.style.backgroundColor = "";
     };
     reader.readAsDataURL(bannerUpload);
   } else {
@@ -67,16 +110,5 @@ document.getElementById("iconUpload").addEventListener("change", (e) => {
     iconReader.readAsDataURL(iconUpload);
   } else {
     headerIcon.style.display = "none";
-  }
-});
-
-document.getElementById("searchBox").addEventListener("change", (e) => {
-  e.preventDefault();
-  const searchBar = document.getElementById("searchBox").value;
-  const search = document.getElementById("headerSearch");
-  if (searchBar === "true") {
-    search.style.display = "block";
-  } else {
-    search.style.display = "none";
   }
 });
